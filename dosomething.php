61cc3fd8b51caa4e4827614637e1d3c1d4755bca
Ayodele <ayodele.owolabi@gmail.com>
2017-03-17 12:55:01 +0100

    We done editing

--- dosomething.php
+++ dosomething.php
@@ -1,10 +1,8 @@
 <?php 
 
-<<<<<<< HEAD
-echo "Some michael";
-=======
-echo "Something you cannot do... i go find trouble";
- 63f3bc0a96ac917fe0855ad34780bacc18177380
- lemme beat you to it
+
+
+echo "Something you cannot do done!";
+
 
 ?>
