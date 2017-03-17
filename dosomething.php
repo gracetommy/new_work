d7cace14c0432c3ecd39d4585e8b3dff87cb9e10
Ayodele <ayodele.owolabi@gmail.com>
2017-03-17 12:42:37 +0100

    more conflicts

--- dosomething.php
+++ dosomething.php
@@ -1,14 +1,6 @@
 <?php 
 
-echo "Something";
-echo "I go spoil things";
-echo "Spoil more things";
-echo "spoil extra things";
-
-echo "pulling pullee";
+echo "Something babknon;oinv;eirnv;iaenldvjnqeljadnvqlejndbfj qenadfbkjvqneildvbqiejnv";
 
 
-echo"starting a group work";
-
-echo "Oga na wa o";
  ?>
